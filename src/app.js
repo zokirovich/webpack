8cfbf036c75dfb8dd9cf8d47e2533d51bd807cc8
Shokirjon <samandarovsakir@gmail.com>
2026-03-03 13:08:42 +0500

    webpack

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// oddiy js da fayillarni o`qib webpacksiz ishlash usuli
+
+// getDate();
+
+// console.log('APP JS')
+
+
---
+++ src/requiest.js
@@ -0,0 +1,5 @@
+// oddiy js da fayillarni o`qib webpacksiz ishlash usuli
+
+// const getDate = () => {
+// 	console.log('get Date')
+// }
